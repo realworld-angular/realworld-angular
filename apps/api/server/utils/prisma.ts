4f93431470c2934ae5024da8ea7904eb49cc7ead
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-07-19 21:38:07 +0200

    feat(api): add prisma optimize

--- apps/api/server/utils/prisma.ts
+++ apps/api/server/utils/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { withOptimize } from "@prisma/extension-optimize";
 
-let _prisma: PrismaClient
+let _prisma;
 
 export const usePrisma = () => {
     if (!_prisma) {
-        _prisma = new PrismaClient()
+        _prisma = new PrismaClient().$extends(withOptimize());
     }
-    return _prisma
+    return _prisma;
 }
