e88a4e032ff5efc45eac0a77d9588ed99bdbfcbf
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-07-29 00:18:56 +0200

    fix(api): remove optimize for Netlify

--- apps/api/server/utils/prisma.ts
+++ apps/api/server/utils/prisma.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-import { withOptimize } from "@prisma/extension-optimize";
 
 let _prisma;
 
 export const usePrisma = () => {
     if (!_prisma) {
-        _prisma = new PrismaClient().$extends(withOptimize());
+        _prisma = new PrismaClient();
     }
     return _prisma;
 }
