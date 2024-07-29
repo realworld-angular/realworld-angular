23c2addcc770f34efd1e37ee1dec9abbc9747aa9
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-07-29 19:39:52 +0200

    refactor(docs): ad analog docs project

---
+++ apps/docs/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideFileRouter } from '@analogjs/router';
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFileRouter(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    provideContent(withMarkdownRenderer(), withShikiHighlighter()),
+  ],
+};
