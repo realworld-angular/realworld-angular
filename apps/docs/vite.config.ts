3de94d4b99bd5c5d566690b1896f5a99e7383725
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-07-29 19:39:52 +0200

    refactor(docs): ad analog docs project

---
+++ apps/docs/vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      content: {
+        highlighter: 'shiki',
+      },
+      prerender: {
+        routes: ['/blog', '/blog/2022-12-27-my-first-post'],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['**/*.spec.ts'],
+    reporters: ['default'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
