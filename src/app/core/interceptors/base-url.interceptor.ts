63670504f7bbb01c3be53fedefeed926e8092441
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2026-05-18 01:41:38 +0200

    Enhance base URL interceptor to include image requests
    - Updated the base URL interceptor to also handle requests starting with '/images', ensuring that image URLs are correctly prefixed with the base API URL.

--- src/app/core/interceptors/base-url.interceptor.ts
+++ src/app/core/interceptors/base-url.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.url.startsWith('/api')) {
+  if (req.url.startsWith('/api') || req.url.startsWith('/images')) {
     const apiReq = req.clone({ url: `${environment.apiBaseUrl}${req.url}` });
     return next(apiReq);
   }
